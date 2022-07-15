5e51a7880ff8bd01bf2e6d6b68e548b73b7fdf9e
jdmunozg <juan.david.m.g1004@gmail.com>
2022-07-15 11:59:02 -0500

    create folder landing page with routing

--- clarisa-front/src/app/app-routing.module.ts
+++ clarisa-front/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"landing-page", 
+    loadChildren: () => import('./landing-page/landing-page.module').then((m) => m.LandingPageModule),
+  },
+  {
+    redirectTo:"landing-page",
+    path:"",
+    pathMatch:"full",
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

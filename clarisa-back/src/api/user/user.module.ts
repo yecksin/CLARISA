c64cc6f5eaa19423d0b422b5ff8b96543a3285cd
JuanCode <jucacar28@hotmail.com>
2022-07-22 16:41:34 -0500

    JC - add ORM config

--- clarisa-back/src/api/user/user.module.ts
+++ clarisa-back/src/api/user/user.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 
-@Module({ controllers: [UserController], providers: [UserService] })
+@Module({
+  controllers: [UserController],
+  providers: [UserService]
+})
 export class UserModule {}
--- clarisa-back/src/app.module.ts
+++ clarisa-back/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RouterModule } from '@nestjs/core';
 import { ApiModule } from './api/api.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {routes} from './routes';
 
+import * as ormconfig from '../ormconfig';
+
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      ...ormconfig, 
+      keepConnectionAlive: true, 
+      autoLoadEntities: true
+    }),
     RouterModule.register(routes),
     ApiModule],
   controllers: [AppController],

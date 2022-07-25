581bff2b694579c288455b5f56996f8041464305
JuanCode <jucacar28@hotmail.com>
2022-07-25 11:54:00 -0500

    JC - Add first endpoint (Users)

--- clarisa-back/src/api/user/entities/user.entity.ts
+++ clarisa-back/src/api/user/entities/user.entity.ts
@@ -1,1 +1,66 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  first_name: string;
+
+  @Column()
+  last_name: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({type: 'tinyint'})
+  is_cgiar_user: boolean;
+
+  @Column()
+  created_by: number; //!Revisar relacion
+
+  @Column()
+  active_since: Date;
+
+  @Column()
+  modified_by: number; //!Revisar relacion
+
+  @Column()
+  modification_justification: string;
+
+  @Column({type: 'tinyint'})
+  is_active: boolean;
+
+  @Column()
+  last_login: Date;
+
+  @Column({type: 'tinyint'})
+  auto_save: boolean;
+
+  @Column({type: 'tinyint'})
+  agree_terms: boolean;
+
+
+  hashPassword(): void {
+    const salt = bcrypt.genSaltSync(10);
+    this.password = bcrypt.hashSync(this.password, salt);
+  }
+
+  checkPassword(password: string): boolean {
+    try {
+      return bcrypt.compareSync(password, this.password);
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+}
--- clarisa-back/src/api/user/user.service.ts
+++ clarisa-back/src/api/user/user.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+
+
+  constructor(@InjectRepository(User)
+  private usersRepository: Repository<User>,){
+
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.usersRepository.find();
   }
 
   findOne(id: number) {

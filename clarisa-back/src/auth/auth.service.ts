463052e0caca042baeac1c41048a84d1e994b234
German Martinez <g.martinez@cgiar.org>
2022-07-27 17:32:28 -0500

    Adding a method to authenticate an user

--- clarisa-back/src/auth/auth.service.ts
+++ clarisa-back/src/auth/auth.service.ts
@@ -3,28 +3,37 @@ import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/api/user/entities/user.entity';
 import { UserService } from 'src/api/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { ModuleRef } from '@nestjs/core';
+import { BaseAuthenticator } from './utils/interface/BaseAuthenticator';
+import { LDAPAuth } from './utils/LDAPAuth';
+import { DBAuth } from './utils/DBAuth';
+import { BaseMessageDTO } from './utils/BaseMessageDTO';
 
 @Injectable()
 export class AuthService {
   constructor(
     private usersService: UserService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private moduleRef: ModuleRef
   ) { }
 
-  async validateUser(email: string, password: string) {
+  async validateUser(email: string, pass: string) {
     email = email.trim().toLowerCase();
-    const user:any = await this.usersService.findOneByEmail(email);
+    const user: User = await this.usersService.findOneByEmail(email)??await this.usersService.findOneByUsername(email);
+    let authenticator : BaseAuthenticator;
     console.log({ user });
 
     // const user_Info = await user.userInfo;
 
     // console.log({user_Info});
     
-    if (user && user.checkIfUnencryptedPasswordIsValid(password)) {
-      //await  this.usersService.updateLastLogin(user);
-      const { password, is_active, ...result } = user;
-      console.log('credentials ok');
-      return result;
+    if (user) {
+      authenticator = this.moduleRef.get(user.is_cgiar_user ? LDAPAuth : DBAuth);
+      let authResult : boolean | BaseMessageDTO = await authenticator.authenticate(email, pass);
+      if(typeof authResult === 'boolean'){
+        console.log('epic', {user});
+        return true;
+      }
     } else {
       throw new HttpException('Credenciales inválidas. Por favor revisa e inténtalo de nuevo.', HttpStatus.UNAUTHORIZED);
     }

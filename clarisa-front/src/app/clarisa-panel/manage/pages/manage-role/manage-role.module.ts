5ee8fc6d2f991be93f1c110a80f4d29609af8ed3
jdmunozg <juan.david.m.g1004@gmail.com>
2022-07-28 09:30:56 -0500

    Create new module and routing

---
+++ clarisa-front/src/app/clarisa-panel/manage/pages/manage-role/manage-role.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManageRoleRoutingModule } from './manage-role-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManageRoleRoutingModule
+  ]
+})
+export class ManageRoleModule { }

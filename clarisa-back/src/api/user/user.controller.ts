cad750ff399d1080a46d609a647f3c36e984ebd1
German Martinez <g.martinez@cgiar.org>
2022-08-02 16:52:37 -0500

    Implementing user pagination #2: service, new interface for the required params

--- clarisa-back/src/api/user/user.controller.ts
+++ clarisa-back/src/api/user/user.controller.ts
@@ -9,19 +9,24 @@ import {
   HttpException,
   HttpStatus,
   Res,
+  Query,
+  UseGuards,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Response } from 'express';
+import { PaginationParams } from 'src/shared/interfaces/pageable';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  async findAll(){
+    return await this.userService.findAll();
   }
 
   @Get('findByEmail/:email')
@@ -34,11 +39,17 @@ export class UserController {
     return await this.userService.findOneByUsername(username); 
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @Get('get/:id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.userService.findOne(id);
   }
 
+  @Get('search')
+  async getUsersPagination(@Query() { offset, limit }: PaginationParams) {
+    return this.userService.getUsersPagination(offset, limit);
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Patch('update')
   async update(@Res() res: Response, @Body() updateUserDtoList: UpdateUserDto[]) {
     try {

807acad156eca822d5933affbc093f8c912ca9fc
German Martinez <g.martinez@cgiar.org>
2022-08-02 16:44:50 -0500

    Adding the class-validation library

--- clarisa-back/src/main.ts
+++ clarisa-back/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as bodyparser from 'body-parser';
 import { AppModule } from './app.module';
@@ -6,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(bodyparser.urlencoded({ limit: '100mb', extended: true }));
   app.use(bodyparser.json({ limit: '100mb' }));
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(3000);
 }

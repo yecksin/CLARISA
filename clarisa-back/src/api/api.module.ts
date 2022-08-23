a11c84aaf43691c566e04040f3bcaefad84106ac
German Martinez <g.martinez@cgiar.org>
2022-08-23 10:49:26 -0500

    Adding route and entry on module for countries and geolocations

--- clarisa-back/src/api/api.module.ts
+++ clarisa-back/src/api/api.module.ts
@@ -16,8 +16,9 @@ import { ProjectedBenefitModule } from './projected-benefit/projected-benefit.mo
 import { ActionAreaOutcomeModule } from './action-area-outcome/action-area-outcome.module';
 import { OutcomeIndicatorModule } from './outcome-indicator/outcome-indicator.module';
 import { ActionAreaOutcomeIndicatorModule } from './action-area-outcome-indicator/action-area-outcome-indicator.module';
-import { SourceModule } from './source/source.module';
 import { SourcesModule } from './sources/sources.module';
+import { CountryModule } from './country/country.module';
+import { GeopositionModule } from './geoposition/geoposition.module';
 @Module({
   controllers: [ApiController],
   providers: [ApiService],
@@ -37,8 +38,9 @@ import { SourcesModule } from './sources/sources.module';
     ActionAreaOutcomeModule,
     OutcomeIndicatorModule,
     ActionAreaOutcomeIndicatorModule,
-    SourceModule,
-    SourcesModule
+    SourcesModule,
+    CountryModule,
+    GeopositionModule
   ],
 })
 export class ApiModule {}
--- clarisa-back/src/api/api.routes.ts
+++ clarisa-back/src/api/api.routes.ts
@@ -13,6 +13,8 @@ import { ProjectedBenefitModule } from './projected-benefit/projected-benefit.mo
 import { ActionAreaOutcomeModule } from './action-area-outcome/action-area-outcome.module';
 import { OutcomeIndicatorModule } from './outcome-indicator/outcome-indicator.module';
 import { ActionAreaOutcomeIndicatorModule } from './action-area-outcome-indicator/action-area-outcome-indicator.module';
+import { CountryModule } from './country/country.module';
+import { GeopositionModule } from './geoposition/geoposition.module';
 
 export const apiRoutes = [
   {
@@ -74,5 +76,13 @@ export const apiRoutes = [
   {
     path: 'action-area-outcome-indicators',
     module: ActionAreaOutcomeIndicatorModule,
+  },
+  {
+    path: 'countries',
+    module: CountryModule,
+  },
+  {
+    path: 'geopositions',
+    module: GeopositionModule,
   },
 ];

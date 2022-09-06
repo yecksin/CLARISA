9d131316786c194e5b32ca628ac03545ea0b9124
German Martinez <g.martinez@cgiar.org>
2022-09-06 15:05:27 -0500

    ♻️ refactor(Cgiar Entity Types): Adding missing conditions to display only the types needed

--- clarisa-back/src/api/cgiar-entity-type/cgiar-entity-type.service.ts
+++ clarisa-back/src/api/cgiar-entity-type/cgiar-entity-type.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CgiarEntityTypeEnum } from 'src/shared/entities/enums/cgiar-entity-types';
 import { FindAllOptions } from 'src/shared/entities/enums/find-all-options';
-import { Repository } from 'typeorm';
+import { FindOptionsWhere, In, Repository } from 'typeorm';
 import { CreateCgiarEntityTypeDto } from './dto/create-cgiar-entity-type.dto';
 import { UpdateCgiarEntityTypeDto } from './dto/update-cgiar-entity-type.dto';
 import { CgiarEntityType } from './entities/cgiar-entity-type.entity';
@@ -13,14 +14,29 @@ export class CgiarEntityTypeService {
     private cgiarEntityTypeRepository: Repository<CgiarEntityType>,
   ) {}
 
+  private readonly defaultTypes = [
+    CgiarEntityTypeEnum.CRP,
+    CgiarEntityTypeEnum.PLATFORM,
+    CgiarEntityTypeEnum.CENTER,
+    CgiarEntityTypeEnum.INITIATIVES,
+    CgiarEntityTypeEnum.ONE_CGIAR_PLATFORM,
+  ];
+
+  private readonly whereClause : FindOptionsWhere<CgiarEntityType> = {
+    id: In(this.defaultTypes),
+  }
+
   async findAll(option : FindAllOptions = FindAllOptions.SHOW_ONLY_ACTIVE) : Promise<CgiarEntityType[]> {
     switch(option){
       case FindAllOptions.SHOW_ALL:
-        return await this.cgiarEntityTypeRepository.find();
+        return await this.cgiarEntityTypeRepository.find({
+          where: this.whereClause,
+        });
       case FindAllOptions.SHOW_ONLY_ACTIVE:
       case FindAllOptions.SHOW_ONLY_INACTIVE:
         return await this.cgiarEntityTypeRepository.find({
           where: {
+            ...this.whereClause,
             is_active: option === FindAllOptions.SHOW_ONLY_ACTIVE,
           }
         });

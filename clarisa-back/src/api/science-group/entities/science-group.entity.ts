b9f02ab4d8d8e8fe9c1173a427698d5bb8a7ec8c
German Martinez <g.martinez@cgiar.org>
2022-09-07 15:40:19 -0500

    ✨ feat(Science Groups): Adding relation with Units

--- clarisa-back/src/api/science-group/entities/science-group.entity.ts
+++ clarisa-back/src/api/science-group/entities/science-group.entity.ts
@@ -1,5 +1,13 @@
+import { Unit } from 'src/api/unit/entities/unit.entity';
 import { AuditableEntity } from 'src/shared/entities/extends/auditable-entity.entity';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('science_groups')
 export class ScienceGroup extends AuditableEntity {
@@ -14,4 +22,14 @@ export class ScienceGroup extends AuditableEntity {
 
   @Column()
   parent_id: number;
+
+  @ManyToOne(() => ScienceGroup, (sc) => sc.children)
+  @JoinColumn({ name: 'parent_id' })
+  parent: Promise<ScienceGroup>;
+
+  @OneToMany(() => ScienceGroup, (u) => u.parent)
+  children: Promise<ScienceGroup[]>;
+
+  @OneToMany(() => Unit, (u) => u.science_group)
+  units: Promise<Unit[]>;
 }

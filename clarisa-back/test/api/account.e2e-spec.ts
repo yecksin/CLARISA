b05d50f4356c00d9d5a5a966646fbd7f05b05426
German Martinez <g.martinez@cgiar.org>
2022-09-14 14:50:44 -0500

    ✅ test: Testing Accounts, Account Types, Initiatives, Projected Benefits, Roles

---
+++ clarisa-back/test/api/account.e2e-spec.ts
@@ -0,0 +1,46 @@
+import { TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { testModule, usePipes } from '../test.module';
+
+describe('Accounts (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await testModule.compile();
+    app = moduleFixture.createNestApplication();
+    usePipes(app);
+    await app.init();
+  });
+
+  //It is tested to return a 200 since it is the get all
+  it('/api/accounts (GET)', () => {
+    return request(app.getHttpServer()).get('/api/accounts').expect(200);
+  });
+
+  //The endpoint is used to search by id, a valid id is sent to it.
+  it('/api/accounts/get/1 (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/api/accounts/get/' + 1)
+      .expect(200)
+      .expect((res) => {
+        const data = res.body;
+        expect(data).toHaveProperty('id');
+        expect(data).toHaveProperty('financial_code');
+        expect(data).toHaveProperty('description');
+        expect(data).toHaveProperty('account_type_id');
+        expect(data).toHaveProperty('parent_id');
+      });
+  });
+
+  //The endpoint is used to search by id, an invalid id is sent to it.
+  it('/api/accounts/get/a (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/api/accounts/get/' + 'a')
+      .expect(400);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});

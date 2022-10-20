ac37fefd7581d0d625ecbbb85b2b6fd73f5c1696
German Martinez <g.martinez@cgiar.org>
2022-10-20 17:40:46 -0500

    ♻️ refactor(Sources): adding relation with Institution Dictionary. Fixing wrong column datatype.

--- clarisa-back/src/api/sources/dto/create-source.dto.ts
+++ clarisa-back/src/api/sources/dto/create-source.dto.ts
@@ -1,7 +1,7 @@
 import { AuditableDto } from 'src/shared/entities/dtos/auditable-dto';
 
 export class CreateSourceDto extends AuditableDto {
-  name: number;
+  name: string;
 
   acronym: string;
 

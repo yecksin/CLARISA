dad376071f2864a7074be94b91424ccb35b5441f
jdmunozg <juan.david.m.g1004@gmail.com>
2022-10-23 17:45:44 -0500

    :recycle: refactor(Return response): Changes response json endpoints

--- clarisa-front/src/app/clarisa-panel/documentation/components/content/content.component.ts
+++ clarisa-front/src/app/clarisa-panel/documentation/components/content/content.component.ts
@@ -16,130 +16,137 @@ export class ContentComponent implements OnInit {
   @Input() informationTable: any;
   arrayColumns = [];
   keysTable = [];
+  responseJsonPrint: any;
   constructor(private _manageApiService: EndpointsInformationService) {}
 
-  ngOnInit(): void {
-    console.log(this.information);
+  ngOnInit(): void {}
+
+  iniciativeEndInformation() {
+    var auxVariable = JSON.parse(this.information.response_json);
+    this.arrayColumns = this.columnsTable(auxVariable.properties);
+    return this.arrayColumns;
   }
 
-  iniciativeEndInformation(): void {
-    this.arrayColumns = [];
-    let contain = JSON.parse(this.information.response_json);
-    for (let i in contain.properties) {
-      if (contain.properties[i].show_in_table == true) {
-        this.arrayColumns[contain.properties[i].order] =
-          contain.properties[i].column_name;
-        this.keysTable[contain.properties[i].order] = i;
-      }
+  returnResponseJson() {
+    var auxVariable = JSON.parse(this.information.response_json);
+    this.responseJsonPrint = this.response_json(
+      auxVariable.properties,
+      auxVariable.object_type,
+      1
+    );
+
+    return this.setCharAt(
+      this.responseJsonPrint,
+      this.responseJsonPrint.lastIndexOf(','),
+      ''
+    );
+  }
+
+  //In this function we organize the response in json type.
+  response_json(listResponse, typeResponse, subObject) {
+    let variable = '';
+    var auxList = listResponse;
+    var countSpace = subObject;
+    if (typeResponse == 'list' && countSpace == 1) {
+      variable = '[ \n { ';
     }
-    console.log(this.informationTable);
-  }
-
-  convertJson() {
-    let request_json = JSON.parse(this.information.response_json);
-    let json_reponse = ' {\n';
-    for (var clave in request_json.properties) {
-      if (request_json.properties[clave].object_type == 'field') {
-        json_reponse +=
-          '\t"' +
-          clave +
-          ' " ' +
-          ': "' +
-          request_json.properties[clave].type +
+    if (typeResponse == 'list' && countSpace != 1) {
+      variable = '[ \n' + '\t'.repeat(countSpace) + '{ ';
+    }
+    if (typeResponse == 'object') {
+      variable = '{';
+    }
+    for (let i in auxList) {
+      if (auxList[i].object_type == 'object') {
+        variable = variable + '\n' + '\t'.repeat(countSpace) + '"' + i + '":';
+        variable =
+          variable +
+          this.response_json(
+            auxList[i].properties,
+            auxList[i].object_type,
+            countSpace + 1
+          );
+      } else if (auxList[i].object_type == 'list') {
+        variable = variable + '\n' + '\t'.repeat(countSpace) + '"' + i + '":';
+        variable =
+          variable +
+          this.response_json(
+            auxList[i].properties,
+            auxList[i].object_type,
+            countSpace + 1
+          );
+      } else {
+        variable =
+          variable +
+          '\n ' +
+          '\t'.repeat(countSpace) +
           '"' +
-          ',\n';
+          i +
+          '" : " ' +
+          auxList[i].type +
+          '", ';
       }
-      if (request_json.properties[clave].object_type == 'object') {
-        json_reponse += '\t"' + clave + '": {' + '\n';
-        for (var subclave in request_json.properties[clave].properties) {
-          json_reponse +=
-            '\t\t"' +
-            subclave +
-            '"' +
-            ': "' +
-            request_json.properties[clave].properties[subclave].type +
-            '"' +
-            ',\n';
+    }
+    if (typeResponse == 'list' && countSpace == 1) {
+      variable = variable + ' \n } \n]';
+    }
+    if (typeResponse == 'list' && countSpace != 1) {
+      variable =
+        variable +
+        ' \n ' +
+        '\t'.repeat(countSpace) +
+        '}\n' +
+        '\t'.repeat(countSpace - 1) +
+        ']';
+    }
+    if (typeResponse == 'object') {
+      variable = variable + '\n ' + '\t'.repeat(countSpace - 1) + '}';
+    }
+    return variable;
+  }
+
+  //With this function delete last character with example the comma (,)
+  setCharAt(str, index, chr) {
+    if (index > str.length - 1) return str;
+    return str.substring(0, index) + chr + str.substring(index + 1);
+  }
+
+  columnsTable(listaAux) {
+    var endpointJsonOnes = listaAux;
+    var columns = [];
+    for (let i in endpointJsonOnes) {
+      if (
+        endpointJsonOnes[i].show_in_table == true &&
+        endpointJsonOnes[i].object_type != 'object' &&
+        endpointJsonOnes[i].object_type != 'list'
+      ) {
+        columns[endpointJsonOnes[i].order] = [
+          endpointJsonOnes[i].column_name,
+          i,
+          endpointJsonOnes[i].object_type,
+        ];
+      }
+      if (endpointJsonOnes[i].object_type == 'object') {
+        if (endpointJsonOnes[i].show_in_table == true) {
+          columns[endpointJsonOnes[i].order] = [
+            endpointJsonOnes[i].column_name,
+            i,
+            endpointJsonOnes[i].object_type,
+            this.columnsTable(endpointJsonOnes[i].properties),
+          ];
         }
-        json_reponse += '\t}\n';
+      }
+      if (endpointJsonOnes[i].object_type == 'list') {
+        if (endpointJsonOnes[i].show_in_table == true) {
+          columns[endpointJsonOnes[i].order] = [
+            endpointJsonOnes[i].column_name,
+            i,
+            endpointJsonOnes[i].object_type,
+            this.columnsTable(endpointJsonOnes[i].properties),
+          ];
+        }
       }
     }
-    json_reponse += '}';
-
-    return json_reponse;
-  }
-
-  customSort(event: SortEvent | any) {
-    event.data.sort((data1: any, data2: any) => {
-      let value1 = data1[event.field];
-      let value2 = data2[event.field];
-      let result = null;
-
-      if (value1 == null && value2 != null) result = -1;
-      else if (value1 != null && value2 == null) result = 1;
-      else if (value1 == null && value2 == null) result = 0;
-      else if (typeof value1 === 'string' && typeof value2 === 'string')
-        result = value1.localeCompare(value2);
-      else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
-
-      return event.order * result;
-    });
-  }
-  exportPdf() {
-    let information = [];
-    let d = new Date();
-    for (let k of this.informationTable) {
-      information.push(Object.values(k));
-    }
-    const doc = new jsPDF();
-    autoTable(doc, {
-      head: [this.arrayColumns],
-      body: information,
-      didDrawPage: (dataArg) => {
-        doc.text('PAGE', dataArg.settings.margin.left, 10);
-      },
-    });
-    doc.save(
-      'clarisa ' +
-        this.information.name +
-        d.getFullYear() +
-        (d.getMonth() + 1) +
-        d.getDate() +
-        d.getHours() +
-        d.getMinutes() +
-        '.pdf'
-    );
-  }
-  exportExcel() {
-    import('xlsx').then((xlsx) => {
-      const worksheet = xlsx.utils.json_to_sheet(this.informationTable);
-      const workbook = { Sheets: { data: worksheet }, SheetNames: ['data'] };
-      const excelBuffer: any = xlsx.write(workbook, {
-        bookType: 'xlsx',
-        type: 'array',
-      });
-      this.saveAsExcelFile(excelBuffer, 'Clarisa ');
-    });
-  }
-
-  saveAsExcelFile(buffer: any, fileName: string): void {
-    let d = new Date();
-    let EXCEL_TYPE =
-      'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
-    let EXCEL_EXTENSION = '.xlsx';
-    const data: Blob = new Blob([buffer], {
-      type: EXCEL_TYPE,
-    });
-    FileSaver.saveAs(
-      data,
-      fileName +
-        this.information.name +
-        d.getFullYear() +
-        (d.getMonth() + 1) +
-        d.getDate() +
-        d.getHours() +
-        d.getMinutes() +
-        EXCEL_EXTENSION
-    );
+    return columns;
   }
 }

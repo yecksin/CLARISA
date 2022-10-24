568f01519c37de86690a8bf1160f4ad8b3098ba6
Laura Chaves <laurachaves00@hotmail.com>
2022-10-24 15:55:06 -0500

    ✨ feat(google analytics): google analytics integration

--- clarisa-front/src/app/app.module.ts
+++ clarisa-front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterEventsModule } from './shared/router-events/router-events.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    RouterEventsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

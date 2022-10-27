772abd3c910d57abbad33f26e297ecf6fd81a59d
jdmunozg <juan.david.m.g1004@gmail.com>
2022-10-27 07:29:33 -0500

    :recycle: refactor(Menu api page): Load change, for more user-friendly navigation

--- clarisa-front/src/app/clarisa-panel/documentation/components/vertical-menu/vertical-menu.component.ts
+++ clarisa-front/src/app/clarisa-panel/documentation/components/vertical-menu/vertical-menu.component.ts
@@ -19,47 +19,31 @@ export class VerticalMenuComponent implements OnInit {
   @Input() urlParams: any;
   constructor(public router: Router) {}
 
-  ngOnInit(): void {
-    let idUl = '#' + this.urlParams.namesubcategory;
+  ngOnInit(): void {}
 
+  ngOnChanges(paramsUrl: SimpleChanges) {
+    let isActive = paramsUrl['urlParams'].currentValue.nameEndpoint;
+    let idUl = '#' + paramsUrl['urlParams'].currentValue.namesubcategory;
     $(document).ready(function () {
+      if (isActive != undefined) {
+        $('.endpoints').removeClass('activeSubMenu');
+        $('#' + isActive).addClass('activeSubMenu');
+      }
+      $('.slide').removeClass('is-expanded');
       $(idUl + 1)
         .addClass('is-expanded')
         .removeClass('hide');
     });
   }
 
-  ngOnChanges(paramsUrl: SimpleChanges) {
-    let isActive = paramsUrl['urlParams'].currentValue.nameEndpoint;
-    $(document).ready(function () {
-      if (isActive != undefined) {
-        $('#' + isActive).addClass('activeSubMenu');
-      }
-    });
-  }
-
   clickMenu(idli: any) {
-    $(document).ready(function () {
-      let element = document.getElementById(idli + 1);
-      if (element.classList.contains('is-expanded') == true) {
-        console.log('entre');
-        $('.slide').removeClass('is-expanded');
+    const box = document.getElementById(idli + 1);
+    if (box != null) {
+      if (box.classList.contains('is-expanded') == true) {
+        box.classList.remove('is-expanded');
       } else {
-        $('.slide').removeClass('is-expanded');
-        $('#' + idli + 1)
-          .addClass('is-expanded')
-          .removeClass('hide');
+        box.classList.add('is-expanded');
       }
-    });
-  }
-
-  reloadComponent(parameter) {
-    let currentRoute = this.router.routerState.snapshot.url;
-    console.log(parameter);
-    this.router.navigate([currentRoute]);
-    setTimeout(() => {
-      this.router.navigate([parameter]);
-    });
-    //console.log(“Reload”);
+    }
   }
 }

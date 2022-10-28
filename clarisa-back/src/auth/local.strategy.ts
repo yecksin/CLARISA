2a9a0f6eb1d37a9313b72efb843d6ad9418d8d24
German Martinez <g.martinez@cgiar.org>
2022-10-28 17:15:20 -0500

    ♻️ refactor(Auth): changed the fields for login

--- clarisa-back/src/auth/local.strategy.ts
+++ clarisa-back/src/auth/local.strategy.ts
@@ -6,11 +6,11 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super({ usernameField: 'email' });
+    super({ usernameField: 'login' });
   }
 
-  async validate(email: string, password: string): Promise<any> {
-    const user = await this.authService.validateUser(email, password);
+  async validate(login: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(login, password);
     if (!user) {
       throw new UnauthorizedException();
     }

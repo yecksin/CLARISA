01ab6085c6515a07e05fec2b87a6d557b3b3b449
German Martinez <g.martinez@cgiar.org>
2023-02-20 15:31:01 -0500

    ✨ feat(OldInsitutions): Adding CRUD (Routing, Entity, DTOs, Custom Repository, Controller, Service, Relations)

--- clarisa-back/src/api/institution-type/entities/institution-type.entity.ts
+++ clarisa-back/src/api/institution-type/entities/institution-type.entity.ts
@@ -9,6 +9,7 @@ import {
 } from 'typeorm';
 import { AuditableEntity } from '../../../shared/entities/extends/auditable-entity.entity';
 import { Institution } from '../../institution/entities/institution.entity';
+import { OldInstitution } from '../../old-institution/entities/old-institution.entity';
 import { PartnerRequest } from '../../partner-request/entities/partner-request.entity';
 
 @Entity('institution_types')
@@ -34,6 +35,9 @@ export class InstitutionType extends AuditableEntity {
 
   @OneToMany(() => Institution, (i) => i.institution_type_object)
   institutions: Promise<Institution[]>;
+
+  @OneToMany(() => OldInstitution, (oi) => oi.institution_type_object)
+  old_institutions: Promise<OldInstitution[]>;
 
   @OneToMany(() => PartnerRequest, (pr) => pr.institution_type_object)
   partner_requests: PartnerRequest[];
--- clarisa-back/src/api/institution/entities/institution-location.entity.ts
+++ clarisa-back/src/api/institution/entities/institution-location.entity.ts
@@ -7,6 +7,7 @@ import {
 } from 'typeorm';
 import { AuditableEntity } from '../../../shared/entities/extends/auditable-entity.entity';
 import { Country } from '../../country/entities/country.entity';
+import { OldInstitution } from '../../old-institution/entities/old-institution.entity';
 import { Institution } from './institution.entity';
 
 @Entity('institution_locations')
@@ -29,6 +30,10 @@ export class InstitutionLocation extends AuditableEntity {
   @ManyToOne(() => Institution, (i) => i.institution_locations)
   @JoinColumn({ name: 'institution_id' })
   institution_object: Institution;
+
+  @ManyToOne(() => OldInstitution, (oi) => oi.institution_locations)
+  @JoinColumn({ name: 'institution_id' })
+  old_institution_object: OldInstitution;
 
   @ManyToOne(() => Country, (c) => c.institution_locations)
   @JoinColumn({ name: 'country_id' })

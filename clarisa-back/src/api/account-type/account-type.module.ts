22a4b910390128f4fc5fa02d900d8156792a955e
German Martinez <g.martinez@cgiar.org>
2023-03-15 11:36:41 -0500

    ♻️ refactor(AccountType): changing the way the mapping is done

--- clarisa-back/src/api/account-type/account-type.module.ts
+++ clarisa-back/src/api/account-type/account-type.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AccountTypeService } from './account-type.service';
 import { AccountTypeController } from './account-type.controller';
 import { AccountTypeRepository } from './repositories/account-type.repository';
+import { AccountTypeMapper } from './mappers/account-type.mapper';
 
 @Module({
   controllers: [AccountTypeController],
-  providers: [AccountTypeService, AccountTypeRepository],
+  providers: [AccountTypeService, AccountTypeRepository, AccountTypeMapper],
+  exports: [AccountTypeMapper],
 })
 export class AccountTypeModule {}
--- clarisa-back/src/api/account-type/account-type.service.ts
+++ clarisa-back/src/api/account-type/account-type.service.ts
@@ -1,43 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { FindAllOptions } from '../../shared/entities/enums/find-all-options';
+import { AccountTypeDto } from './dto/account-type.dto';
 import { UpdateAccountTypeDto } from './dto/update-account-type.dto';
 import { AccountType } from './entities/account-type.entity';
+import { AccountTypeMapper } from './mappers/account-type.mapper';
 import { AccountTypeRepository } from './repositories/account-type.repository';
 
 @Injectable()
 export class AccountTypeService {
-  constructor(private accountTypesRepository: AccountTypeRepository) {}
+  constructor(
+    private _accountTypeRepository: AccountTypeRepository,
+    private _accountTypeMapper: AccountTypeMapper,
+  ) {}
 
   async findAll(
     option: FindAllOptions = FindAllOptions.SHOW_ONLY_ACTIVE,
-  ): Promise<AccountType[]> {
-    switch (option) {
-      case FindAllOptions.SHOW_ALL:
-        return await this.accountTypesRepository.find();
-      case FindAllOptions.SHOW_ONLY_ACTIVE:
-      case FindAllOptions.SHOW_ONLY_INACTIVE:
-        return await this.accountTypesRepository.find({
-          where: {
-            auditableFields: {
-              is_active: option === FindAllOptions.SHOW_ONLY_ACTIVE,
-            },
-          },
-        });
-      default:
-        throw Error('?!');
+  ): Promise<AccountTypeDto[]> {
+    if (!Object.values<string>(FindAllOptions).includes(option)) {
+      throw Error('?!');
     }
+
+    const accounts: AccountType[] =
+      await this._accountTypeRepository.findAllAccountTypes(option);
+    const accountTypeDtos: AccountTypeDto[] = accounts.map((a) =>
+      this._accountTypeMapper.classToDto(a),
+    );
+
+    return accountTypeDtos;
   }
 
-  async findOne(id: number): Promise<AccountType> {
-    return await this.accountTypesRepository.findOneBy({
-      id,
-      auditableFields: { is_active: true },
+  async findOne(id: number): Promise<AccountTypeDto> {
+    const result: AccountType = await this._accountTypeRepository.findOne({
+      where: {
+        id,
+        auditableFields: { is_active: true },
+      },
     });
+
+    return this._accountTypeMapper.classToDto(result);
   }
 
   async update(
     updateUserDtoList: UpdateAccountTypeDto[],
   ): Promise<AccountType[]> {
-    return await this.accountTypesRepository.save(updateUserDtoList);
+    return await this._accountTypeRepository.save(updateUserDtoList);
   }
 }

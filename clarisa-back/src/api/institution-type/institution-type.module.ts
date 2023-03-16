0c7304951c8021e625e56d381c8fb4ce1c7a1f4d
German Martinez <g.martinez@cgiar.org>
2023-03-16 14:37:25 -0500

    ✨ feat(Institution Type): new endpoint to get all with a plain structure

--- clarisa-back/src/api/institution-type/institution-type.module.ts
+++ clarisa-back/src/api/institution-type/institution-type.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { InstitutionTypeService } from './institution-type.service';
 import { InstitutionTypeController } from './institution-type.controller';
 import { InstitutionTypeRepository } from './repositories/institution-type.repository';
+import { InstitutionTypeMapper } from './mappers/institution-type.mapper';
 
 @Module({
   controllers: [InstitutionTypeController],
-  providers: [InstitutionTypeService, InstitutionTypeRepository],
-  exports: [InstitutionTypeRepository],
+  providers: [
+    InstitutionTypeService,
+    InstitutionTypeRepository,
+    InstitutionTypeMapper,
+  ],
+  exports: [InstitutionTypeRepository, InstitutionTypeMapper],
 })
 export class InstitutionTypeModule {}
